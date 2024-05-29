51fadbdab3021fdbf7665df916001e1677f61447
fradethiago86 <thiagomaiafrade@gmail.com>
2024-05-29 07:51:16 -0300

    New try to develop something, and it runs. I don't know exactly which meaning this code has, but I believe I'm learning how to work with java once I thought that some programming things I relation one each other, and follow the help from Intellij.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+//**
+// * @author ThiagoMaia
+   //
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+    int x = 0;
+    int y = 1;
+    int z = 0;
+        JOptionPane.showInputDialog("O valor da divisão é: ");{
+        System.out.println("x");
+        x = Integer.parseInt(JOptionPane.showInputDialog("X"));
+
+    }
+    }
+}
