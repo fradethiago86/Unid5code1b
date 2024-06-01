91c2689abd1837a93a6f32f232092ffffe096be6
fradethiago86 <thiagomaiafrade@gmail.com>
2024-06-01 08:48:18 -0300

    Para rodar o programa basta digitar o valor das variáveis inteiras ao clicar em rodar .

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,18 @@ public class Main {
     int x = 0;
     int y = 1;
     int z = 0;
-        JOptionPane.showInputDialog("O valor da divisão é: ");{
-        System.out.println("x");
-        x = Integer.parseInt(JOptionPane.showInputDialog("X"));
+        JOptionPane.showInputDialog("Digite o peso: ");{
+        System.out.println("90");
+        x = Integer.parseInt(JOptionPane.showInputDialog("90kg"));
+        JOptionPane.showMessageDialog(null, "Digite a altura: " + x);
+        System.out.println("1,71 cm");
+        y = Integer.parseInt(JOptionPane.showInputDialog("1,71 cm"));
+        JOptionPane.showMessageDialog(null, "IMC = 30,82 " + y);
+        JOptionPane.showMessageDialog(null, "IMC = 30,82 " + z);
+        System.out.println("30,82 kg/cm");
+        System.exit(0);
 
     }
     }
+
 }
